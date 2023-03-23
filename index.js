265849779fbc6965affd2055eb26ab63a1b0876f
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-03-23 11:47:01 +0100

    fetch and post
    adding the fetch and post functions but can't access the server to get the authorization to put in the header

---
+++ index.js
@@ -0,0 +1,38 @@
+const Base_ULR = "https://striveschool-api.herokuapp.com/api/product";
+const endpoint = "https://striveschool-api.herokuapp.com/api/product";
+
+
+
+
+
+
+
+fetch('https://striveschool-api.herokuapp.com/api/product')
+    .then(response => {
+        // handle the response
+    })
+    .catch(error => {
+        // handle the error
+    });
+
+
+
+
+
+async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
